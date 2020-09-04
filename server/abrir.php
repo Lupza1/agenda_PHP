5ee04ad41da7a58e4a164dba5faef9175b2fe0e2
Lupza1 <59704851+Lupza1@users.noreply.github.com>
2020-09-04 19:30:08 -0300

    third commit

---
+++ server/abrir.php
@@ -0,0 +1,16 @@
+<?php
+
+    $host="localhost";
+    $user='root';
+    $pass='';
+    $data='php_agenda';
+
+    $tab1="usuarios";
+
+    $conn = new mysqli($host,$user,$pass,$data);
+    if($conn->connect_errno){
+        echo "Error: ".$conn->connect_error;
+        exit();
+    }
+
+?>
--- server/check_login.php
+++ server/check_login.php
@@ -1,6 +1,23 @@
 <?php
 
+  SESSION_start();
 
+  include ('../server/abrir.php');
 
+  if (!empty($_POST['user']) && !empty($_POST['password'])){
+    $records = $conn->prepare('SELECT id, email, pass FROM $tab1 WHERE email=:email');
+    $records->bindParams(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
 
- ?>
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      $_SESSION['user_id'] = $results['id'];
+      header('Locationn: /login.php');
+    }else {
+      $message = 'Lo sentimos, el email o la contraseña son incorrectos';
+    }
+  }
+
+?>
